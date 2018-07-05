3220dd232a2c82cd765d2d1e88c73fe8398fc057
Adrian Schawalder <adrian.schawalder@adcubum.com>
2018-07-05 14:33:53 +0200

    test

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,7 +1,9 @@
 package org.leanpoker.player;
 
 import java.math.BigDecimal;
+import java.util.Iterator;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -21,15 +23,32 @@ public class Player {
     }
     
     
-    
     public static void main (String[] args)
     {
-    	String test = "{ \"tournament_id\":\"550d1d68cd7bd10003000003\",  \"game_id\":\"550da1cb2d909006e90004b1\",  \"round\":0,  \"bet_index\":0,  \"small_blind\": 10,  \"current_buy_in\": 320,  \"pot\": 400,  \"minimum_raise\": 240,   \"dealer\": 1,   \"orbits\": 7,   \"in_action\": 1,  \"players\": [  {  \"id\": 0,  \"name\": \"Albert\",  \"status\": \"active\",      \"version\": \"Default random player\",  \"stack\": 1010,   \"bet\": 320  }, { \"id\": 1,  \"name\": \"Bob\", \"status\": \"active\", \"version\": \"Default random player\", \"stack\": 1590, \"bet\": 80, \"hole_cards\": [   { \"rank\": \"6\",  \"suit\": \"hearts\"  }, { \"rank\": \"K\", \"suit\": \"spades\" } ] }, { \"id\": 2, \"name\": \"Chuck\", \"status\": \"out\", \"version\": \"Default random player\", \"stack\": 0, \"bet\": 0 } ], \"community_cards\": [  { \"rank\": \"4\", \"suit\": \"spades\" }, { \"rank\": \"A\", \"suit\": \"hearts\" }, { \"rank\": \"6\", \"suit\": \"clubs\" } ]}";
+    	String current_buy_in = "320";
+    	String test = "{ \"tournament_id\":\"550d1d68cd7bd10003000003\",  \"game_id\":\"550da1cb2d909006e90004b1\",  \"round\":0,  \"bet_index\":0,  \"small_blind\": 10,  \"current_buy_in\": "+current_buy_in+ " ,  \"pot\": 400,  \"minimum_raise\": 240,   \"dealer\": 1,   \"orbits\": 7,   \"in_action\": 1,  \"players\": [  {  \"id\": 0,  \"name\": \"Japsn\",  \"status\": \"active\",      \"version\": \"Default random player\",  \"stack\": 1010,   \"bet\": 320  }, { \"id\": 1,  \"name\": \"Bob\", \"status\": \"active\", \"version\": \"Default random player\", \"stack\": 1590, \"bet\": 80, \"hole_cards\": [   { \"rank\": \"6\",  \"suit\": \"hearts\"  }, { \"rank\": \"K\", \"suit\": \"spades\" } ] }, { \"id\": 2, \"name\": \"Chuck\", \"status\": \"out\", \"version\": \"Default random player\", \"stack\": 0, \"bet\": 0 } ], \"community_cards\": [  { \"rank\": \"4\", \"suit\": \"spades\" }, { \"rank\": \"A\", \"suit\": \"hearts\" }, { \"rank\": \"6\", \"suit\": \"clubs\" } ]}";
     	
     	JsonElement elemnt = new JsonParser().parse(test);
     	JsonObject obj = elemnt.getAsJsonObject();
     	JsonElement element = obj.get("current_buy_in");
     	BigDecimal value = element.getAsBigDecimal();
+    	JsonElement players = obj.get("players");
+    	JsonArray playersList = players.getAsJsonArray();
+
+    	
+    	for (Iterator<JsonElement> itr = playersList.iterator() ; itr.hasNext ();) {
+    		JsonElement jElement = itr.next();
+    		JsonObject player = elemnt.getAsJsonObject();
+    		String name  = player.get("name").getAsString();
+    		if ("japsn".equalsIgnoreCase(name)) {
+    			JsonArray cards = player.get("hole_cards").getAsJsonArray();
+    			
+    			for (Iterator<JsonElement> itrCards = cards.iterator() ; itrCards.hasNext ();) {
+    				JsonElement card = itrCards.next();	
+    			}
+    		}
+    		
+    	}
     	//betRequets ();
     }
 }
