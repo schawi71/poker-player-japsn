d9c65d0d134599a2d1f60995481ca28a7060679a
Adrian Schawalder <adrian.schawalder@adcubum.com>
2018-07-05 15:59:15 +0200

    1.5

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -12,7 +12,7 @@ import com.google.gson.JsonParser;
 
 public class Player {
 
-    static final String VERSION = "V 1.4";
+    static final String VERSION = "V 1.5";
 
     public static int betRequest(JsonElement request) {
     	JsonObject obj = request.getAsJsonObject();
